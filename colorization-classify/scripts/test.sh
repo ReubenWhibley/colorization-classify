a1e32003f05f92059679be30791ae63416dcd2be
ReubenWhibley <46591079+ReubenWhibley@users.noreply.github.com>
2021-02-24 14:54:26 +0800

    Add files via upload

---
+++ colorization-classify/scripts/test.sh
@@ -0,0 +1,5 @@
+/home/reuben/Desktop/colorizationMaster/caffe_private_pascal/build/tools/caffe test \
+-model=/home/reuben/Desktop/colorization-classify/model/anet_train_val.prototxt \
+-weights=/media/reuben/Samsung_T5/imgnet/cifar/cifar_iter_150.caffemodel \
+-iterations=100 \
+-gpu 0
---
+++ colorization-classify/scripts/train.sh
@@ -0,0 +1,4 @@
+/home/reuben/Desktop/colorizationMaster/caffe_private_pascal/build/tools/caffe train \
+-solver /home/reuben/Desktop/colorization-classify/model/solver.prototxt \
+-weights /home/reuben/Desktop/colorization-classify/model/alexnet_release_450000_nobn_rs.caffemodel \
+-gpu 0
